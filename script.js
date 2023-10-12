1571f18bf544045b2042695ae1a3a624f309c89c
Swad-ahmed <144508201+Swad-ahmed@users.noreply.github.com>
2023-10-12 21:07:42 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+
 function rock() {
     let cs = 'You Choose Rock'
     document.getElementById('choise').innerHTML = cs
     var pcc = Math.floor(Math.random ()* 3)
 
-   if (pcc === 0) {
-    document.getElementById("pcc").innerHTML = "You lose"
-   }
-   else{
-    document.getElementById("pcc").innerHTML = "You win"
-   }
+    if (pcc === 1) {
+        document.getElementById("pcc").innerHTML = "You lose"
+        document.getElementById("img").src ='Paper.png'
+       }
+   
+       else{
+        if (pcc === 0){
+            document.getElementById("img").src ='Scissors.png'
+        }
+        if (pcc === 2){
+            document.getElementById("img").src ='Rock.png'
+            document.getElementById("pcc").innerHTML= "Draw"
+        }
+        else{
+        document.getElementById("pcc").innerHTML = "You win"  
+        }   
+       }
 }
 function paper() {
     let cs = 'You Choose Paper'
@@ -16,22 +28,49 @@ function paper() {
     var pcc = Math.floor(Math.random ()* 3)
     if (pcc === 1) {
         document.getElementById("pcc").innerHTML = "You lose"
+        document.getElementById("img").src ='Scissors.png'
        }
+   
        else{
-        document.getElementById("pcc").innerHTML = "You win"
+        if (pcc === 0){
+            document.getElementById("img").src ='Rock.png'
+        }
+        if (pcc === 2){
+            document.getElementById("img").src ='Paper.png'
+            document.getElementById("pcc").innerHTML= "Draw"
+        }
+        else{
+        document.getElementById("pcc").innerHTML = "You win"  
+        }   
        }
 }
 function Scissors() {
     let cs = 'You Choose Scissors'
     document.getElementById('choise').innerHTML = cs
     var pcc = Math.floor(Math.random ()* 3)
-    if (pcc === 2) {
+    if (pcc === 1) {
         document.getElementById("pcc").innerHTML = "You lose"
+        document.getElementById("img").src ='Rock.png'
        }
+   
        else{
-        document.getElementById("pcc").innerHTML = "You win"
+        if (pcc === 0){
+            document.getElementById("img").src ='Paper.png'
+        }
+        if (pcc === 2){
+            document.getElementById("img").src ='Scissors.png'
+            document.getElementById("pcc").innerHTML= "Draw"
+        }
+        else{
+        document.getElementById("pcc").innerHTML = "You win"  
+        }   
        }
 }
+
+
+
+
+
 
 
 
